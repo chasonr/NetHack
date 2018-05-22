25f9134954dbdbdcedc43081966902205a11f9b8
Ray Chason <ray.chason@protonmail.com>
2018-05-22 01:43:36 -0400

    Add optional PNG support to tileset.c

--- win/share/bmptiles.c
+++ win/share/bmptiles.c
@@ -107,15 +107,6 @@ struct TileSetImage *image;
     if (!read_info_header(fp, &header2)) goto error;
     if (!check_info_header(&header2)) goto error;
 
-#if 0 /* TODO */
-    if (header2.Compression == BI_PNG) {
-        /* Image data is an embedded PNG bit stream */
-        boolean ok = do_read_png_tiles(fp, image));
-        fclose(fp);
-        return ok;
-    }
-#endif
-
     /* header2.Height < 0 means the Y coordinate is reversed; the origin is
      * top left rather than bottom left */
     image->width = header2.Width;
