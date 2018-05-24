31efa1f796e03def19a3b94c36b202b5047a082e
Ray Chason <ray.chason@protonmail.com>
2018-05-24 00:24:00 -0400

    Make all stubs conditional on their window ports

--- sys/winnt/stubs.c
+++ sys/winnt/stubs.c
@@ -17,6 +17,8 @@ struct window_procs sdl2_procs = { "guistubs" };
 #endif
 #ifdef QT_GRAPHICS
 struct window_procs Qt_procs = { "guistubs" };
+#endif
+#ifdef CURSES_GRAPHICS
 struct window_procs curses_procs = { "guistubs" };
 int qt_tilewidth, qt_tileheight, qt_fontsize, qt_compact_mode;
 #endif
