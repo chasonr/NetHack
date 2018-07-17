d090fcbc795cf6df7718db5c393fe50589525c97
Ray Chason <ray.chason@protonmail.com>
2018-07-16 20:48:26 -0400

    Build Qt5 and SDL2 interfaces under Wine and MinGW

---
+++ win/Qt4/qt4stub.c
@@ -0,0 +1,9 @@
+/* stub definitions of Qt 4 variables. Used by the win32 port when building
+ * the console-mode binary. */
+
+#include <stddef.h>
+
+char *qt_tilewidth=NULL;
+char *qt_tileheight=NULL;
+char *qt_fontsize=NULL;
+int qt_compact_mode = 0;
